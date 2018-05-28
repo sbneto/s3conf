dd2286263996531c653d48f197c4ed89b3e90239
Samuel <samuel.m.b.neto@gmail.com>
2018-05-28 11:49:07 -0300

    sections detection is now in the exception

--- s3conf/exceptions.py
+++ s3conf/exceptions.py
@@ -1,3 +1,28 @@
+from click.exceptions import UsageError
+
+from . import config
+
+
+class EnvfilePathNotDefinedUsageError(UsageError):
+    def __init__(self, message=None, *args, **kwargs):
+        message = message or ''
+        if message:
+            message += '\n'
+        super().__init__(message, *args, **kwargs)
+
+        error_msg = 'Set the environemnt variable S3CONF or provide a section from an existing config file.'
+        try:
+            sections_detected = ''
+            for section in config.ConfigFileResolver(config.LOCAL_CONFIG_FILE).sections():
+                sections_detected += '    {}\n'.format(section)
+        except FileNotFoundError:
+            pass
+        if sections_detected:
+            sections_detected = '\n\nThe following sections were detected:\n\n' + sections_detected
+
+        self.message += error_msg + sections_detected
+
+
 class EnvfilePathNotDefinedError(Exception):
     pass
 
