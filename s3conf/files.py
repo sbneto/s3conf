63c6cf6374bf34f97aa82b3f6265cc0057aac905
Samuel <samuel.m.b.neto@gmail.com>
2018-05-28 11:25:12 -0300

    adding set and unset methods to EnvFile, adding helper function to parse env file line

--- s3conf/files.py
+++ s3conf/files.py
@@ -11,6 +11,24 @@ logger = logging.getLogger(__name__)
 __escape_decoder = codecs.getdecoder('unicode_escape')
 
 
+def parse_env_var(value):
+    """
+    Split a env var text like
+
+    ENV_VAR_NAME=env_var_value
+
+    into a tuple ('ENV_VAR_NAME', 'env_var_value')
+    """
+    k, _, v = value.partition('=')
+
+    # Remove any leading and trailing spaces in key, value
+    k, v = k.strip(), v.strip().encode('unicode-escape').decode('ascii')
+
+    if v and v[0] == v[-1] in ['"', "'"]:
+        v = __escape_decoder(v[1:-1])[0]
+    return k, v
+
+
 class File:
     def __init__(self, name, storage=None):
         self.name = name
@@ -40,12 +58,11 @@ class File:
     def edit(self, create=False):
         with NamedTemporaryFile(mode='rb+', buffering=0) as f:
             original_data = b''
-            if not self.exists():
-                if not create:
-                    logger.error('Can not edit file %s. It does not exits', self.name)
-                    raise FileDoesNotExist(self.name)
-            else:
+            try:
                 original_data = self.read()
+            except FileDoesNotExist:
+                if not create:
+                    raise
             f.write(original_data)
             edited_data = editor.edit(filename=f.name)
             if edited_data != original_data:
@@ -59,21 +76,50 @@ class EnvFile(File):
     def as_dict(self):
         env_dict = {}
         try:
-            for line in str(self.read(), 'utf-8').splitlines():
-                line = line.strip()
-                if line and not line.startswith('#') and '=' in line:
-                    k, _, v = line.partition('=')
-
-                    # Remove any leading and trailing spaces in key, value
-                    k, v = k.strip(), v.strip().encode('unicode-escape').decode('ascii')
-
-                    if v and v[0] == v[-1] in ['"', "'"]:
-                        v = __escape_decoder(v[1:-1])[0]
-
-                    env_dict[k] = v
+            lines = str(self.read(), 'utf-8').splitlines()
+            lines = [line for line in lines if line and not line.startswith('#') and '=' in line]
+            env_dict = dict(parse_env_var(line) for line in lines)
         except FileNotFoundError:
             pass
         return env_dict
+
+    def set(self, value, create=False):
+        new_key, new_value = parse_env_var(value)
+        new_lines = []
+        value_set = False
+        try:
+            for line in str(self.read(), 'utf-8').splitlines():
+                key, value = parse_env_var(line)
+                if key == new_key:
+                    new_lines.append('{}={}'.format(new_key, new_value))
+                    value_set = True
+                else:
+                    new_lines.append(line)
+        except FileNotFoundError:
+            if not create:
+                raise
+        if not value_set:
+            new_lines.append('{}={}'.format(new_key, new_value))
+        self.write('\n'.join(new_lines))
+
+    def unset(self, unset_key):
+        new_lines = []
+        unset_done = False
+        try:
+            for line in str(self.read(), 'utf-8').splitlines():
+                key, value = parse_env_var(line)
+                if key == unset_key:
+                    unset_done = True
+                    continue
+                else:
+                    new_lines.append(line)
+        except FileNotFoundError:
+            logger.warning('File does not exist')
+
+        if unset_done:
+            self.write('\n'.join(new_lines))
+        else:
+            logger.info('Key %s not found in environemnt file, doing nothing...', unset_key)
 
     def from_dict(self, env_vars):
         file_data = ''
