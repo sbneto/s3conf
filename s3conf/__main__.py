4663465d4ad496ff74cfd5e315397c31d96cc6d6
sbneto <samuel.m.b.neto@gmail.com>
2018-09-12 12:40:44 -0300

    adding comment about __main__.py

--- s3conf/__main__.py
+++ s3conf/__main__.py
@@ -6,6 +6,7 @@ from .client import main as _main
 def main():
     # fixing the name of the entrypoint when the module is executed as
     # python -m s3conf
+    # https://docs.python.org/3/library/__main__.html
     entrypoint_name = os.path.basename(sys.argv[0])
     if entrypoint_name == '__main__.py':
         entrypoint_name = 's3conf'
