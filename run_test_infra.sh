67e0850a01940be1be6af9bf42e174fadbeb060c
sbneto <samuel.m.b.neto@gmail.com>
2018-10-11 16:16:27 -0300

    pinning localstack version, using only S3

--- run_test_infra.sh
+++ run_test_infra.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-docker run -d --rm -e LOCALSTACK_HOSTNAME="localhost" -p 8080:8080 -p 443:443 -p 4567-4583:4567-4583 -p 4590-4593:4590-4593 -v "/tmp/localstack:/tmp/localstack" -v "/var/run/docker.sock:/var/run/docker.sock" -e DOCKER_HOST="unix:///var/run/docker.sock" -e HOST_TMP_FOLDER="/tmp/localstack" "localstack/localstack"
+docker run \
+    -d \
+    --rm \
+    -e LOCALSTACK_HOSTNAME="localhost" \
+    -e SERVICES="s3" \
+    -p 8080:8080 \
+    -p 443:443 \
+    -p 4572:4572 \
+    -p 4590-4593:4590-4593 \
+    -v "/tmp/localstack:/tmp/localstack" \
+    -v "/var/run/docker.sock:/var/run/docker.sock" \
+    -e DOCKER_HOST="unix:///var/run/docker.sock" \
+    -e HOST_TMP_FOLDER="/tmp/localstack" \
+    "localstack/localstack:0.8.7"
