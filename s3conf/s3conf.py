0fdbc9054b2778cf186312b5f5859544f27e4d18
Samuel <samuel.m.b.neto@gmail.com>
2019-04-28 20:55:36 -0300

    moving path adjustments to config

--- s3conf/s3conf.py
+++ s3conf/s3conf.py
@@ -2,9 +2,8 @@ import os
 import codecs
 import logging
 import json
-from shutil import rmtree
 
-from .utils import prepare_path, md5s3
+from .utils import prepare_path
 from . import exceptions, files, storages, config
 
 logger = logging.getLogger(__name__)
@@ -26,27 +25,11 @@ def parse_dotenv(data):
             yield k, v
 
 
-def unpack_list(files_list):
-    files_pairs = files_list.split(';') if files_list else []
-    files_map = []
-    for file_map in files_pairs:
-        file_source, _, file_target = file_map.rpartition(':')
-        if file_source and file_target:
-            files_map.append((file_source, file_target))
-    return files_map
-
-
 def phusion_dump(environment, path):
     prepare_path(path if path.endswith('/') else path + '/')
     for k, v in environment.items():
         with open(os.path.join(path, k), 'w') as f:
             f.write(v + '\n')
-
-
-def change_root_dir(file_path, root_dir=None):
-    if root_dir:
-        file_path = os.path.join(root_dir, file_path.lstrip('/'))
-    return file_path
 
 
 def expand_path(path, path_target):
@@ -79,30 +62,13 @@ class S3Conf:
         self.settings = settings or config.Settings()
         self.storage = storage or storages.S3Storage(settings=self.settings)
 
-    @property
-    def environment_file_path(self):
-        # resolving environment file path
-        file_name = self.settings.get('S3CONF')
-        if not file_name:
-            logger.error('Environemnt file name is not defined or is empty.')
-            raise exceptions.EnvfilePathNotDefinedError()
-        return file_name
-
-    def diff(self, local_root):
-        local_environment = change_root_dir(os.path.basename(self.environment_file_path).lstrip('/'), local_root)
-        with open(local_environment) as f:
-            diff = self.get_envfile().diff(f)
-        return diff
-
     def push(self, force=False):
         if not force:
             md5_hash_file_name = os.path.join(self.settings.cache_dir, 'md5')
             hashes = json.load(open(md5_hash_file_name))
 
-        file_map_list = unpack_list(self.settings.get('S3CONF_MAP'))
         if not force:
-            for remote_path, local_path in file_map_list:
-                local_path = change_root_dir(local_path, self.settings.root_folder)
+            for local_path, remote_path in self.settings.file_mappings.items():
                 mapping = expand_path(local_path, remote_path)
                 for local_file, remote_file in mapping:
                     current_hash = hashes.get(local_file)
@@ -113,32 +79,18 @@ class S3Conf:
                         logger.warning('New mapped file detected: %s', local_file)
 
         hashes = {}
-        hashes.update(self.upload_mapping(file_map_list, root_dir=self.settings.root_folder))
-
+        for local_path, remote_path in self.settings.file_mappings.items():
+            hashes.update(self.upload(local_path, remote_path))
         md5_hash_file_name = os.path.join(self.settings.cache_dir, 'md5')
         json.dump(hashes, open(md5_hash_file_name, 'w'), indent=4)
         return hashes
 
     def pull(self):
-        hashes = self.download_mapping(self.settings.get('S3CONF_MAP'), root_dir=self.settings.root_folder)
+        hashes = {}
+        for local_path, remote_path in self.settings.file_mappings.items():
+            hashes.update(self.download(remote_path, local_path))
         md5_hash_file_name = os.path.join(self.settings.cache_dir, 'md5')
         json.dump(hashes, open(md5_hash_file_name, 'w'), indent=4)
-        return hashes
-
-    def download_mapping(self, files, root_dir=None):
-        if isinstance(files, str):
-            files = unpack_list(files)
-        hashes = {}
-        for remote_file, local_file in files:
-            hashes.update(self.download(remote_file, change_root_dir(local_file, root_dir)))
-        return hashes
-
-    def upload_mapping(self, files, root_dir=None):
-        hashes = {}
-        if isinstance(files, str):
-            files = unpack_list(files)
-        for remote_file, local_file in files:
-            hashes.update(self.upload(change_root_dir(local_file, root_dir), remote_file))
         return hashes
 
     def download(self, path, path_target, force=False):
@@ -150,7 +102,7 @@ class S3Conf:
             else:
                 target_name = path_target
             prepare_path(target_name)
-            target_file = files.File(target_name)
+            target_file = storages.LocalStorage(self.settings).open(target_name)
             existing_md5 = target_file.md5() if target_file.exists() and not force else None
             if not existing_md5 or existing_md5 != md5hash:
                 source_name = os.path.join(path, file_path).rstrip('/')
@@ -172,8 +124,8 @@ class S3Conf:
         return hashes
 
     def get_envfile(self):
-        logger.info('Loading configs from {}'.format(self.environment_file_path))
-        return files.EnvFile.from_file(self.storage.open(self.environment_file_path))
+        logger.info('Loading configs from {}'.format(self.settings.environment_file_path))
+        return files.EnvFile.from_file(self.storage.open(self.settings.environment_file_path))
 
     def edit(self, create=False):
-        files.EnvFile.from_file(self.storage.open(self.environment_file_path)).edit(create=create)
+        files.EnvFile.from_file(self.storage.open(self.settings.environment_file_path)).edit(create=create)
