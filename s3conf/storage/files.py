5f62e8781a8e7195003b226485a2f138b4593954
Samuel <nervsam@gmail.com>
2019-05-06 20:17:38 -0300

    some logging

--- s3conf/storage/files.py
+++ s3conf/storage/files.py
@@ -33,6 +33,7 @@ class File:
     def flush(self) -> None:
         self.file.flush()
         self._buffer.seek(0)
+        logger.debug('Writing buffer to %s', self.name)
         self.storage.write(self._buffer, self.name)
 
     # IOBase
