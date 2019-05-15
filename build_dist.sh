f6fcdb5083ece29b356125c948bfa1c732476f70
Samuel <nervsam@gmail.com>
2019-05-15 17:40:28 -0300

    fixing client name in travis script

--- build_dist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-pip install -r requirements.txt
-pyinstaller pyinstaller.py --name s3conf-$(uname -s)-$(uname -m) --onefile --hidden-import=configparser --additional-hooks-dir=hooks
-rm -rf build s3conf-$(uname -s)-$(uname -m).spec
