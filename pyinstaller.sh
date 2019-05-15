d2d40c8dc226ef2499622eeb5c8bbe84820356e0
Samuel <nervsam@gmail.com>
2019-05-15 15:05:14 -0300

    trying out osx builds on travis

--- pyinstaller.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf dist build s3conf.spec
-pyinstaller pyinstaller.py --name s3conf --onefile --hidden-import=configparser --additional-hooks-dir=hooks
-rm -rf build s3conf.spec
