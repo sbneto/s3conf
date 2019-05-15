4cfc501ee79abe1d2bb4aef53b99b79a05995bdb
Samuel <nervsam@gmail.com>
2019-05-15 17:32:52 -0300

    better naming and adding readme

--- build_dist.sh
+++ build_dist.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-OS_NAME=$1
-
 pip install -r requirements.txt
-pyinstaller pyinstaller.py --name s3conf --onefile --hidden-import=configparser --additional-hooks-dir=hooks
-rm -rf build s3conf.spec
-cd dist
-tar czf s3conf.$OS_NAME.tar.gz s3conf
-cd ..
+pyinstaller pyinstaller.py --name s3conf-$(uname -s)-$(uname -m) --onefile --hidden-import=configparser --additional-hooks-dir=hooks
+rm -rf build s3conf-$(uname -s)-$(uname -m).spec
